cbe0344554ccd86b4264e075511aeaf3370f53db
juankamilon <j.narvaezo@uniandes.edu.co>
2023-09-26 11:41:38 -0500

    descargar bases de datos

---
+++ code/code.R
@@ -0,0 +1,15 @@
+#Juan Kamilo Narvaez - 202113808
+#"R version 4.3.1 (2023-06-16 ucrt)"
+
+## instalar/llamar pacman
+install.packages("pacman")
+library(pacman)
+require(pacman)
+
+## usar la función p_load de pacman para instalar/llamar las librerías de la clase
+p_load(rio,
+       skimr,
+       janitor,
+       data.table,
+       tidyverse)
+
